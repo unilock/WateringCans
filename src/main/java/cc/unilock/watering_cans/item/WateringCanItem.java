8b10123a370b56e6096fe82f73558a5f1ea3539b
unilock <unilock@fennet.rentals>
2025-03-09 10:47:03 -0400

    Slight optimization to config check
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/cc/unilock/watering_cans/item/WateringCanItem.java
+++ src/main/java/cc/unilock/watering_cans/item/WateringCanItem.java
@@ -75,13 +75,13 @@ public class WateringCanItem extends Item {
 					}
 
 					if ((this.getMaxUseTime(stack) - remainingUseTicks + 1) % this.rate == 0) {
-						boolean fertilizableCfg = CONFIG.fertilizable.value();
+						boolean skipFertilizable = !CONFIG.fertilizable.value();
 						for (BlockPos pos : BlockPos.iterateOutwards(blockHitResult.getBlockPos(), this.range, 1, this.range)) {
 							if (isMoisturizable(world, pos)) {
 								moisturize(world, pos);
 							}
 
-							if (isTickable(world, pos, fertilizableCfg)) {
+							if (isTickable(world, pos, skipFertilizable)) {
 								tick(world, pos);
 							}
 						}
@@ -109,8 +109,8 @@ public class WateringCanItem extends Item {
 		world.setBlockState(pos, world.getBlockState(pos).with(FarmlandBlock.MOISTURE, 7), Block.NOTIFY_LISTENERS);
 	}
 
-	private static boolean isTickable(World world, BlockPos pos, boolean fertilizableCfg) {
-		return world.getBlockState(pos).hasRandomTicks() && (!fertilizableCfg || (world.getBlockState(pos).getBlock() instanceof Fertilizable fertilizable && fertilizable.isFertilizable(world, pos, world.getBlockState(pos), world.isClient)));
+	private static boolean isTickable(World world, BlockPos pos, boolean skipFertilizable) {
+		return world.getBlockState(pos).hasRandomTicks() && (skipFertilizable || (world.getBlockState(pos).getBlock() instanceof Fertilizable fertilizable && fertilizable.isFertilizable(world, pos, world.getBlockState(pos), world.isClient)));
 	}
 
 	private static void tick(World world, BlockPos pos) {
